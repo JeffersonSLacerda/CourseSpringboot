6fa12891f69cfc0d8b7673bb0ba6cf0546ccebf1
JeffersonSLacerda <jefferson.lacerdasi@outlook.com>
2023-12-28 09:47:51 -0300

    H2 database, test profile, jpa

--- src/main/java/com/lacerdadev/course/entities/User.java
+++ src/main/java/com/lacerdadev/course/entities/User.java
@@ -3,9 +3,16 @@ package com.lacerdadev.course.entities;
 import java.io.Serializable;
 import java.util.UUID;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
   private static final long serialVersionUID = 1L;
 
+  @Id
   private UUID id;
   private String name;
   private String email;
